6246036f8b64ed9c1cea196084b47994a40e788f
JamilShoujah <jws09@mail.aub.edu>
2025-05-21 15:00:11 +0300

    initial push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace jamil_dotnet_microsoft_certificate.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
